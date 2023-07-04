fcad55d4b943a8aa28a54617e40f3cca88c5a09a
HakamRaza <ismatcd@yahoo.com>
2023-07-04 16:27:39 +0800

    wip: order controller

--- Qs02-RewardPoints/app/controller/orderController.php
+++ Qs02-RewardPoints/app/controller/orderController.php
@@ -3,43 +3,89 @@
 namespace App\Controller;
 
 use App\DTO\OrderDTO;
+use App\Enums\OrderStatus;
+use App\Enums\TransactionType;
+use App\Helper\Response;
 use App\Model\RewardPoints;
 use App\Model\OrderSale;
+use Exception;
 
 /**
  * 
- * @method placeOrder
+ * @method orderPlace
+ * @method orderPaid
+ * @method orderDelivered
  */
 class OrderController
 {
-    public function __construct(
-        protected OrderSale $order,
-        protected RewardPoints $reward
-    ) {
+    protected $order;
+    protected $reward;
+    protected $response;
+
+    public function __construct()
+    {
+        $this->order = new OrderSale();
+        $this->reward = new RewardPoints();
+        $this->response = new Response();
+    }
+
+    /**
+     * Registed new order
+     */
+    public function orderPlace(OrderDTO $orderPayload)
+    {
+        // any point claim ?
+        $pointClaimed = $orderPayload->pointClaimed;
+        
+        // check existing point
+        if ($pointClaimed > 0) {
+            $currentPoint = $this->reward->available();
+
+            if ($currentPoint < $pointClaimed) {
+                throw new Exception('Reward points not enough to be claimed.');
+            }
+        }
+
+        // update status save order
+        $orderPayload->setOrderStatusId(OrderStatus::Pending->value());
+        $order = $this->order->create($orderPayload);
+        var_dump($order);
+        die();
+
+
+
+        // else {
+        //     $this->reward->create($orderPayload->userId, TransactionType::Debit, $pointClaimed);
+        // }
     }
 
 
-
-    /**
-     * 
-     */
-    public function create(OrderDTO $orderPayload)
+    public function cancel(OrderDTO $orderPayload)
     {
-        // save order
-
-
-
-
         // calculate points
 
-
         // assign points
-
 
         //
     }
 
-    public function update(OrderDTO $orderPayload)
+    public function paid(OrderDTO $orderPayload)
+    {
+        // calculate points
+
+        // assign points
+
+        //
+    }
+
+    public function complete(OrderDTO $orderPayload)
+    {
+    }
+
+    /**
+     * Get effective debit/credit point to be claim for each order
+     */
+    private function calculateEffectivePointClaim(float $totalSales, float $pointClaimed)
     {
     }
 }
