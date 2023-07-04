e23807b93888350339320f57c055188f6ea324f6
HakamRaza <ismatcd@yahoo.com>
2023-07-04 18:57:13 +0800

    feat Main

--- Qs02-RewardPoints/main.php
+++ Qs02-RewardPoints/main.php
@@ -3,82 +3,64 @@
 
 use App\Controller\OrderController;
 use App\Dto\OrderDTO;
-use App\Enums\Currency;
 use App\Helper\Conversion;
-use App\Helper\Response;
+use App\Migration\DBInit;
 
 require_once __DIR__ . './vendor/autoload.php';
 
-
-// use App\Controller\OrderController;
-// use App\Helper\Conversion;
-// use App\Migration\DBInit;
-
-// use App\Migration\DBInit;
-// use App\Model\Currency;
-
-
-
-
-
-// $init = new DBInit();
-// $init->migrateTable();
-
-
-// $conversion = new Conversion();
-// $result = $conversion->convertIntToDecimal(1111);
-// var_dump($result);
-
-// $currency = new Currency();
-// $result = $currency->findCurrency('MYR');
-// var_dump($result['conversion_rate_usd']);
-
 class Main
 {
-    // public function init()
-    // {
-    //     $init = new DBInit();
-    //     $init->migrateTable();
-    // }
     private $controller;
-    private $response;
     private $conversion;
 
     public function __construct()
     {
         $this->controller = new OrderController();
-        $this->response = new Response();
         $this->conversion = new Conversion();
     }
 
-    public function add()
+    public function initDB()
     {
-        $dateTime = new DateTime();
-        $newOrder = new OrderDTO();
-        $newOrder->setCurrencyId(Currency::MYR->value());
-        $newOrder->setTotalSales($this->conversion->convertDecimalToInt(167.89));
-        $newOrder->setUserId(1);
-        $newOrder->setUpdatedAt($dateTime);
-        $newOrder->setCreatedAt($dateTime);
-        $newOrder->setPointClaimed($this->conversion->convertDecimalToInt(90));
-
-        try {
-            $order = $this->controller->place($newOrder);
-            return $this->response->sendResponse($order);
-        } catch (\Throwable $th) {
-
-            var_dump($th->getMessage());
-            return $this->response->sendResponse('Failed to place order', 500);
-        }
+        $init = new DBInit();
+        $init->migrateTable();
     }
 
+    public function placeOrder(
+        int $userId,
+        int $currencyId,
+        float $amountOrder,
+        float $amountPointClaim = 0.00,
+    ) {
+        $dateTime = new DateTime();
+        $newOrder = new OrderDTO();
+        $newOrder->setCurrencyId($currencyId);
+        $newOrder->setTotalSales($this->conversion->convertDecimalToInt($amountOrder));
+        $newOrder->setUserId($userId);
+        $newOrder->setUpdatedAt($dateTime);
+        $newOrder->setCreatedAt($dateTime);
+        $newOrder->setPointClaimed($this->conversion->convertDecimalToInt($amountPointClaim));
 
-    public function acceptOrder(int $orderId)
+        return $this->controller->place($newOrder);
+    }
+
+    public function cancelOrder(int $orderId)
     {
+        return $this->controller->cancel($orderId);
+    }
+
+    public function payOrder(int $orderId)
+    {
+        return $this->controller->paid($orderId);
+    }
+
+    public function deliveredOrder(int $orderId)
+    {
+        return $this->controller->complete($orderId);
     }
 }
 
-(new Main())->add();
+// (new Main())->placeOrder(1, 2, 100.00, 10.00);
+// (new Main())->deliveredOrder(1);
 
 
 ?>
