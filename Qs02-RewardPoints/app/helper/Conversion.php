306d98f8dd30bfc3a7f4458d1a3ed9a26a0e4a37
HakamRaza <ismatcd@yahoo.com>
2023-07-04 16:27:27 +0800

    wip: add helper class

--- Qs02-RewardPoints/app/helper/Conversion.php
+++ Qs02-RewardPoints/app/helper/Conversion.php
@@ -1,34 +1,31 @@
-<?
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+use App\Model\Currency;
 
 class Conversion
 {
-
-    public static function convertIntToDecimal(float $value): float
+    public function convertIntToDecimal(float $value): float
     {
         return $value * (1 / 100);
     }
 
 
-    public static function convertDecimalToInt(float $value)
+    public function convertDecimalToInt(float $value): float
     {
         return $value * 100;
     }
 
 
-    public function convertToUSD(string $currencyIn, float $value)
+    public function convertToUSD(string $currencyIn, float $value): float
     {
         $intVal = $this->convertDecimalToInt($value);
+        $result = (new Currency())->findCurrency($currencyIn);
+        $converted = $intVal / floatval($result['conversion_rate_usd']);
 
-        
+        return $this->convertIntToDecimal($converted);
     }
-
-
-
-    
 }
-
-
-
-
-
-?>
