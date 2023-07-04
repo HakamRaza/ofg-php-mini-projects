80b785107d3af380a77e1c923717c731ff592fe8
HakamRaza <ismatcd@yahoo.com>
2023-07-04 18:55:53 +0800

    feat Conversion

--- Qs02-RewardPoints/app/helper/Conversion.php
+++ Qs02-RewardPoints/app/helper/Conversion.php
@@ -19,13 +19,41 @@ class Conversion
         return $value * 100;
     }
 
-
-    public function convertToUSD(string $currencyIn, float $value): float
+    public function convertCurrencyToUSD(int $currencyId, int $value): float|null
     {
-        $intVal = $this->convertDecimalToInt($value);
-        $result = (new Currency())->findCurrency($currencyIn);
-        $converted = $intVal / floatval($result['conversion_rate_usd']);
+        $result = (new Currency())->findCurrency($currencyId);
+
+        if (!$result) return null;
+
+        $converted = $value / floatval($result['conversion_rate_usd']);
 
         return $this->convertIntToDecimal($converted);
+    }
+
+    /**
+     * 1 pt = USD 0.01
+     * i.e. If user want to claim USD 1.00, he needs 100 pts
+     * All point stored in DB already converted to USD * 100
+     */
+    public function convertPointClaimToPointDBRate(int $currencyId, int $pointClaimed)
+    {
+        $UsdRate = $this->convertCurrencyToUSD($currencyId, $pointClaimed);
+
+        if (!$UsdRate) return null;
+
+        return $UsdRate * 100;
+    }
+
+    /**
+     * i.e If user paid USD 100 complete, get 100 pts
+     * All point stored in DB already converted to USD * 100
+     */
+    public function convertPaymentToPointDBRate(int $currencyId, int $totalSales)
+    {
+        $UsdRate = $this->convertCurrencyToUSD($currencyId, $totalSales);
+
+        if (!$UsdRate) return null;
+
+        return $UsdRate * 1;
     }
 }
