d7f2c2c48bb25d9fbc6d4395ed7a8c926467620e
HakamRaza <ismatcd@yahoo.com>
2023-07-04 16:27:07 +0800

    wip: add models

--- Qs02-RewardPoints/app/model/RewardPoints.php
+++ Qs02-RewardPoints/app/model/RewardPoints.php
@@ -2,18 +2,59 @@
 
 namespace App\Model;
 
-// use Utils\DBService;
+use App\Enums\TransactionType;
+use App\Helper\DB;
 
 class RewardPoints
 {
-    public function create() {
-        // $stmt = $this->runQuery("SELECT * FROM $tableName");
-    
-        // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        //     fputcsv($file_handle, $row);
-        // }
-        
+    protected $db;
+    protected $conversion;
+    private $tableName = 'point_rewards';
 
+    public function __construct()
+    {
+        $this->db = new DB();
     }
-    
+
+    /**
+     * Get nett reward points value
+     */
+    public function available()
+    {
+        $query = 'SELECT SUM(
+            CASE 
+                WHEN transaction_type_id = :creditId THEN points 
+                WHEN transaction_type_id = :debitId THEN -points 
+                ELSE +0
+            END
+        ) AS available
+        from `' . $this->tableName . '` WHERE expired_at > NOW();';
+
+        $statement = $this->db->prepare($query);
+
+        $statement->execute([
+            'creditId' => TransactionType::Credit->value(),
+            'debitId' => TransactionType::Debit->value(),
+        ]);
+
+        return (int) $statement->fetch()["available"];
+    }
+
+    /**
+     * Add reward point transaction records
+     * 
+     */
+    public function create(int $userId, TransactionType $type, int $pointClaim): bool
+    {
+        $query = 'INSERT INTO `' . $this->tableName . '` (user_id, transaction_type_id, points, expired_at, created_at) 
+        VALUES (:userId, :transactionTypeId, :points, DATE_ADD(NOW(), INTERVAL 1 YEAR), NOW());';
+
+        $statement = $this->db->prepare($query);
+
+        return $statement->execute([
+            "userId" => $userId,
+            "transactionTypeId" => $type->value(),
+            "points" => $pointClaim
+        ]);
+    }
 }
